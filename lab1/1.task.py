1fd5eb8c662dacf42be7ec960c899acaf3ecfa83
dianaabdirahmanova711-tech <dianaabdirahmanova711@gmail.com>
2026-02-07 08:12:23 +0300

    task1

--- lab1/1.task.py
+++ lab1/1.task.py
@@ -2,3 +2,33 @@ print("hello world")
 #2
 print("Zere+Aidos=<3"
       "")
+#3
+def analyze_text(text):
+    text1 = text.lower()
+    text2 = ""
+    for alpha in text1:
+        if alpha.isalpha() or alpha == " ":
+            text2 += alpha
+    dauysty = "aeiouy"
+    zhana_text = ""
+    for alpha in text2:
+        if alpha in dauysty:
+            zhana_text += alpha
+    text3 = text2.split()
+    result = ""
+    text4 = ''
+    for word in text3:
+        if len(word) >= 5:
+            if word[0] == word[-1]:
+                if word not in result:
+                    result += word + " "
+                    text4 += word + "," + " "
+
+    return {
+        "дауысты әріптер:" : set(zhana_text),
+    "дауысты әріптер саны:": len(set(zhana_text)),
+        "слова длиной ≥ 5:": text4,
+        "жауабы:": result
+    }
+kerek_matyn = input("керек мәтінді жазыңыз:")
+print(analyze_text(kerek_matyn))
